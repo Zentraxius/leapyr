6266cfd9423f6017381143f3ecab9b95cff51747
Cody Fritz <clanalia55@gmail.com>
2020-06-09 19:19:17 -0700

    add UIF and begin entering BL

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('form#leap-year').submit(function(event) {
+    event.preventDefault();
+    const year = parseInt($('input#year').val());
+    const result = leapYear(year);
+    $('#result').text(result);
+  })
+})
+
+  /////////////////////////////////////////
+ /////  User Interface Logic Above   /////
+/////////////////////////////////////////
+
+function leapYear(year) {
+  return false;
+}
