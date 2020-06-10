b6aa903752928b875e5898f4d66ac19388454f4e
Cody Fritz <clanalia55@gmail.com>
2020-06-09 20:41:06 -0700

    finished lesson, going to sleep, suffering has ended

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
-  $('form#leap-year').submit(function(event) {
+  $("form#leap-year").submit(function(event) {
     event.preventDefault();
-    const year = parseInt($('input#year').val());
-    const result = leapYear(year);
-    $('#result').text(result);
-  })
-})
 
+    const year = parseInt($("input#year").val());
+    const result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // Result === false but in greek
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
   /////////////////////////////////////////
  /////  User Interface Logic Above   /////
 /////////////////////////////////////////
 
 function leapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
